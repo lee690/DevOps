fb08c8075a0f858a472fe47801039e9ded460122
lee690 <chrisleejean@gmail.com>
2024-02-21 14:18:39 -0500

    Edit docker and testing, to use Selenium chrome

--- spec/unit/page_spec.rb
+++ spec/unit/page_spec.rb
@@ -1,5 +1,24 @@
 require 'capybara'
 require 'capybara/dsl'
+require 'selenium-webdriver'
+
+include Capybara::DSL
+Capybara.app_host = "http://website"
+Capybara.run_server = false
+Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app,
+        :browser => :remote,
+        :url => "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+        :capabilities => Selenium::WebDriver::Options.chrome(
+            "goog:chromeOptions" => {"args" => [
+                '--no-default-browser-check',
+                '--disable-dev-shm'
+            ] }
+        )
+    )
+end
+Capybara.default_driver = :selenium
+
 
 describe "Example page render unit tests" do
     it "should show the Explore California logo" do
